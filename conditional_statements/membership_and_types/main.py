b682874cae4e2e40b62ea587d3dabe27645bc3e8
klamkin344 <klamkin24@gmail.com>
2026-04-19 17:24:04 +0300

    Update main.py

--- conditional_statements/membership_and_types/main.py
+++ conditional_statements/membership_and_types/main.py
@@ -20,3 +20,4 @@ print("Contains 'raw':", contains_raw)
 print("Contains 'Imported':", contains_Imported)
 print("Is price a float?:", price_is_float)
 print("Is count an integer?:", count_is_int)
+run:main.py
